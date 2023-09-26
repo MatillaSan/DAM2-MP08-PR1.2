6ba1c518b6e30ea2a1ff26f1c5043a59075b56b4
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2023-09-26 18:33:51 +0200

    Done: -Created checkButton, textBox -Convert textBox to Integer -Check if the number is correct. ToDo: -Create AlertDialog -Create TextView -Reset the game -Create fails count

--- app/src/main/java/com/example/pr12/MainActivity.java
+++ app/src/main/java/com/example/pr12/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.pr12;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        final Button checkButton = findViewById(R.id.checkButton);
+        EditText textBox = findViewById(R.id.textBox);
+        final int winNum = rand.nextInt(100);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer usrNum = Integer.parseInt(String.valueOf(textBox.getText()));
+
+                if (usrNum > winNum) {
+                    Toast toast = Toast.makeText(MainActivity.this, "Your number is higher!", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else if (usrNum < winNum) {
+                    Toast toast = Toast.makeText(MainActivity.this, "Your number is lower!", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(MainActivity.this, "You win!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+
+            }
+        });
     }
 }
