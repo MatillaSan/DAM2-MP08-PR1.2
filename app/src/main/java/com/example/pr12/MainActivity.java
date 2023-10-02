584c2f5a415aa61c09d013640b66b9541f373425
MatillaSan <dsanchezmatilla.cf@iesesteveterradas.cat>
2023-10-02 20:12:53 +0200

    Need to solve scrollBar problem, the rest is done.

--- app/src/main/java/com/example/pr12/MainActivity.java
+++ app/src/main/java/com/example/pr12/MainActivity.java
@@ -2,44 +2,88 @@ package com.example.pr12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    Random rand = new Random();
 
+    int numberCount = 0;
+    int winNum = rand.nextInt(100);
+    String numbersUsed = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Random rand = new Random();
         final Button checkButton = findViewById(R.id.checkButton);
         EditText textBox = findViewById(R.id.textBox);
-        final int winNum = rand.nextInt(100);
+        TextView textAttemptCount = findViewById(R.id.attempsNumber);
+        TextView textNumbersUsed = findViewById(R.id.textNumbersUseds);
+        HorizontalScrollView scrollUsedNumbers = findViewById(R.id.scrollNumbers);
         checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Integer usrNum = Integer.parseInt(String.valueOf(textBox.getText()));
+                if (!textBox.getText().toString().equals("")) {
+                    Integer usrNum = Integer.parseInt(String.valueOf(textBox.getText()));
 
-                if (usrNum > winNum) {
-                    Toast toast = Toast.makeText(MainActivity.this, "Your number is higher!", Toast.LENGTH_SHORT);
-                    toast.show();
-                } else if (usrNum < winNum) {
-                    Toast toast = Toast.makeText(MainActivity.this, "Your number is lower!", Toast.LENGTH_SHORT);
-                    toast.show();
-                } else {
-                    Toast toast = Toast.makeText(MainActivity.this, "You win!", Toast.LENGTH_SHORT);
-                    toast.show();
+                    if (usrNum > winNum) {
+                        Toast toast = Toast.makeText(MainActivity.this, "Your number is higher!", Toast.LENGTH_SHORT);
+                        toast.show();
+                        numberCount++;
+                        numbersUsed += usrNum.toString() + " higher - ";
+                        textAttemptCount.setText("Attemps Number: " + numberCount);
+                        textNumbersUsed.setText(numbersUsed + "                   ");
+                        scrollUsedNumbers.fullScroll(ScrollView.FOCUS_RIGHT);
+                        textBox.setText("");
+
+                    } else if (usrNum < winNum) {
+                        Toast toast = Toast.makeText(MainActivity.this, "Your number is lower!", Toast.LENGTH_SHORT);
+                        toast.show();
+                        numberCount++;
+                        numbersUsed += usrNum.toString() + " lower - ";
+                        textAttemptCount.setText("Attemps Number: " + numberCount);
+                        textNumbersUsed.setText(numbersUsed + "                   ");
+                        scrollUsedNumbers.fullScroll(ScrollView.FOCUS_RIGHT);
+                        textBox.setText("");
+
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("You has won the game!")
+                                .setTitle("Congratulations!")
+                                .setPositiveButton("Play Again", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        textBox.setText("");
+                                        winNum = rand.nextInt(100);
+                                        numberCount = 0;
+                                        textAttemptCount.setText("Attemps Number: " + 0);
+                                        numbersUsed = "";
+                                        textNumbersUsed.setText("");
+                                    }
+                                })
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        finish();
+                                        System.exit(0);
+                                    }
+                                });
+
+                        AlertDialog dialog = builder.create();
+
+                        dialog.show();
+
+                    }
                 }
-
-
             }
         });
     }
